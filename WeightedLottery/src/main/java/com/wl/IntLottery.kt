9e1f74c980763d1b6fa76733f868ee118b944b1b
Guy Kobrinsky <gkobrinsky@outbrain.com>
2019-02-06 15:00:39 +0200

    add comments

--- WeightedLottery/src/main/java/com/wl/IntLottery.kt
+++ WeightedLottery/src/main/java/com/wl/IntLottery.kt
@@ -1,9 +1,14 @@
 package com.wl
 
 interface IntLottery {
+
+
+    // get random index, according to the given distribution. throw NoSuchElementException if no items left
     fun draw(): Int
 
+    // return true in case there are no items to draw, false otherwise
     fun empty(): Boolean
 
+    // return the number of remaining valid calls to 'draw'
     fun remaining(): Int
 }
