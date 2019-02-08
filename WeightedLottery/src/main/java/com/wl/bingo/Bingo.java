dd9074819479ec6dbb615a0d5299515a1f6131ab
Amir Hadadi <ahadadi@outbrain.com>
2019-02-08 14:33:12 +0200

    Implement weighted lottery without repetitions using a partial sums binary tree.

---
+++ WeightedLottery/src/main/java/com/wl/bingo/Bingo.java
@@ -0,0 +1,33 @@
+package com.wl.bingo;
+
+import com.wl.IntLottery;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Bingo implements IntLottery {
+  private IntLotteryDelegator delegator;
+
+  public Bingo(final double[] weights, final ThreadLocalRandom random) {
+    delegator = new PartialSumsTree(weights, new boolean[weights.length], weights.length, random);
+  }
+
+  @Override
+  public int draw() {
+    if (empty()) {
+      throw new NoSuchElementException();
+    }
+    delegator = delegator.delegate();
+    return delegator.draw();
+  }
+
+  @Override
+  public boolean empty() {
+    return delegator.empty();
+  }
+
+  @Override
+  public int remaining() {
+    return delegator.remaining();
+  }
+}
