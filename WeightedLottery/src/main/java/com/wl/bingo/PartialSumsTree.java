cdfffc57a4177fde59a458389f95e9ea396d8bca
Amir Hadadi <ahadadi@outbrain.com>
2019-02-09 21:46:57 +0200

    Avoid redundant copy of internal nodes during init.

--- WeightedLottery/src/main/java/com/wl/bingo/PartialSumsTree.java
+++ WeightedLottery/src/main/java/com/wl/bingo/PartialSumsTree.java
@@ -17,18 +17,19 @@ public class PartialSumsTree implements IntLotteryDelegator {
     // We add a dummy element at index 0 to simplify child indexing.
     // We also make the tree full by adding a dummy last element in case #weights is even.
     this.tree = new double[n + 2 - n % 2];
-    System.arraycopy(weights, 0, tree, 1, n);
+    final int firstLeafIndex = n >> 1;
+    System.arraycopy(weights, firstLeafIndex, tree, 1 + firstLeafIndex, n - firstLeafIndex);
 
     // We piggy back on the tree creation to verify the weights instead of using a dedicated loop.
-    for (int i = n >>> 1; i > n >>> 2; i--) {
+    for (int i = n >> 1; i > n >> 2; i--) {
       final int left = i << 1;
-      tree[i] = verify(tree[i]) + verify(tree[left]) + verify(tree[left + 1]);
+      tree[i] = verify(weights[i - 1]) + verify(tree[left]) + verify(tree[left + 1]);
     }
 
     // Same loop as above, but we need to verify only internal nodes.
-    for (int i = n >>> 2; i > 0; i--) {
+    for (int i = n >> 2; i > 0; i--) {
       final int left = i << 1;
-      tree[i] = verify(tree[i]) + tree[left] + tree[left + 1];
+      tree[i] = verify(weights[i - 1]) + tree[left] + tree[left + 1];
     }
 
     this.random = random;
@@ -52,7 +53,7 @@ public class PartialSumsTree implements IntLotteryDelegator {
 
     double r = random.nextDouble(tree[1]);
     int root = 1;
-    while (root <= n >>> 1) {
+    while (root <= n >> 1) {
       final int left = root << 1;
       if (r < tree[left]) {
         root = left;
@@ -81,7 +82,7 @@ public class PartialSumsTree implements IntLotteryDelegator {
   private void remove(int root) {
     final double rootWeight = weights[root - 1];
     selected[root - 1] = true;
-    for (; root >= 1; root >>>= 1) {
+    for (; root >= 1; root >>= 1) {
       tree[root] -= rootWeight;
     }
     size--;
